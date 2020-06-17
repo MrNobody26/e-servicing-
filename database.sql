fed363bca5cce64b6230d54d81d26d4a27372b30
MrNobody26 <shivrajj814@gmail.com>
2020-06-17 18:21:20 +0530

    final commit

--- database.sql
+++ database.sql
@@ -1,25 +1,25 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 31, 2020 at 08:22 AM
--- Server version: 5.1.53
--- PHP Version: 5.3.4
+-- Host: 127.0.0.1
+-- Generation Time: Jun 17, 2020 at 02:50 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `database`
 --
-CREATE DATABASE `database` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `database`;
 
 -- --------------------------------------------------------
 
@@ -27,20 +27,14 @@ USE `database`;
 -- Table structure for table `address`
 --
 
-CREATE TABLE IF NOT EXISTS `address` (
-  `ad_id` int(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `address` (
+  `ad_id` int(100) NOT NULL,
   `c_id` int(100) NOT NULL,
   `p_phone` int(10) NOT NULL,
   `c_city` varchar(50) NOT NULL,
   `street` varchar(50) NOT NULL,
-  `pin` int(6) NOT NULL,
-  PRIMARY KEY (`ad_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `address`
---
-
+  `pin` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -48,21 +42,38 @@ CREATE TABLE IF NOT EXISTS `address` (
 -- Table structure for table `appointment`
 --
 
-CREATE TABLE IF NOT EXISTS `appointment` (
-  `booking_id` int(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `appointment` (
+  `booking_id` int(100) NOT NULL,
   `c_id` int(100) NOT NULL,
   `car_no` varchar(10) NOT NULL,
   `time slot avl` time NOT NULL,
-  `req_date` date NOT NULL,
-  PRIMARY KEY (`booking_id`),
-  UNIQUE KEY `booking_id_2` (`booking_id`),
-  KEY `booking_id` (`booking_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `req_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `appointment`
+-- Table structure for table `appointments`
 --
 
+CREATE TABLE `appointments` (
+  `id` int(11) NOT NULL,
+  `appointment_date` date DEFAULT NULL,
+  `appointment_time` time DEFAULT NULL,
+  `is_available` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `appointments`
+--
+
+INSERT INTO `appointments` (`id`, `appointment_date`, `appointment_time`, `is_available`) VALUES
+(1, '2020-06-18', '16:00:00', 1),
+(2, '2020-06-18', '18:00:00', 1),
+(5, '2020-06-19', '10:00:00', 0),
+(6, '2020-06-19', '12:00:00', 1),
+(7, '2020-06-19', '14:00:00', 0),
+(8, '2020-06-18', '16:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -70,17 +81,11 @@ CREATE TABLE IF NOT EXISTS `appointment` (
 -- Table structure for table `bill`
 --
 
-CREATE TABLE IF NOT EXISTS `bill` (
+CREATE TABLE `bill` (
   `total amount` int(50) NOT NULL,
   `bill no` int(10) NOT NULL,
-  `bill description` varchar(100) NOT NULL,
-  PRIMARY KEY (`bill no`)
+  `bill description` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `bill`
---
-
 
 -- --------------------------------------------------------
 
@@ -88,7 +93,7 @@ CREATE TABLE IF NOT EXISTS `bill` (
 -- Table structure for table `car_details`
 --
 
-CREATE TABLE IF NOT EXISTS `car_details` (
+CREATE TABLE `car_details` (
   `car_no` varchar(50) NOT NULL,
   `car type` varchar(50) NOT NULL,
   `car_name` varchar(50) NOT NULL,
@@ -111,16 +116,15 @@ INSERT INTO `car_details` (`car_no`, `car type`, `car_name`, `car_model`, `c_id`
 -- Table structure for table `customer`
 --
 
-CREATE TABLE IF NOT EXISTS `customer` (
+CREATE TABLE `customer` (
   `First_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `c_contact` int(50) NOT NULL,
   `c_email` varchar(20) NOT NULL,
   `c_address` varchar(50) NOT NULL,
   `c_password` varchar(10) NOT NULL,
-  `c_id` int(50) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`c_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=78 ;
+  `c_id` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `customer`
@@ -167,19 +171,13 @@ INSERT INTO `customer` (`First_name`, `last_name`, `c_contact`, `c_email`, `c_ad
 -- Table structure for table `product`
 --
 
-CREATE TABLE IF NOT EXISTS `product` (
-  `p_id` int(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `product` (
+  `p_id` int(100) NOT NULL,
   `c_id` int(100) NOT NULL,
   `name` varchar(50) NOT NULL,
   `type` varchar(20) NOT NULL,
-  `amount` int(10) NOT NULL,
-  PRIMARY KEY (`p_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `product`
---
-
+  `amount` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -187,15 +185,96 @@ CREATE TABLE IF NOT EXISTS `product` (
 -- Table structure for table `service_type`
 --
 
-CREATE TABLE IF NOT EXISTS `service_type` (
+CREATE TABLE `service_type` (
   `c_id` int(100) NOT NULL,
   `repair` varchar(100) NOT NULL,
   `washing` int(100) NOT NULL,
-  `spare parts` int(100) NOT NULL,
-  PRIMARY KEY (`c_id`)
+  `spare parts` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `service_type`
+-- Indexes for dumped tables
 --
 
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`ad_id`);
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`booking_id`),
+  ADD UNIQUE KEY `booking_id_2` (`booking_id`),
+  ADD KEY `booking_id` (`booking_id`);
+
+--
+-- Indexes for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bill`
+--
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`bill no`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- Indexes for table `service_type`
+--
+ALTER TABLE `service_type`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `address`
+--
+ALTER TABLE `address`
+  MODIFY `ad_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `booking_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `c_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `p_id` int(100) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
